fba7484f435f55cb2962eb2b006ac3db21b0cfd1
Marius Schmidt <marius@denktmit.de>
2022-02-12 14:08:54 +0100

    Setup JaCoCo and full test support

--- src/test/java/de/denktmit/tam/webapp/WebappApplicationIT.java
+++ src/test/java/de/denktmit/tam/webapp/WebappApplicationIT.java
@@ -3,11 +3,14 @@ package de.denktmit.tam.webapp;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest
-class WebappApplicationTests {
+class WebappApplicationIT {
 
 	@Test
 	void contextLoads() {
+		assertThat("").isEqualTo("");
 	}
 
 }
